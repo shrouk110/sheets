b84a6bff54f2bd35117ced5df453e788055d50ea
shrouk110 <saranasser59177@gmail.com>
2025-07-11 12:01:07 +0300

    Time: 0 ms (100%), Space: 8 MB (58.9%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int first = 1;  // ways to reach step 1
+        int second = 2; // ways to reach step 2
+        int total;
+
+        for (int i = 3; i <= n; i++) {
+            total = first + second; // ways to reach step i
+            first = second;
+            second = total;
+        }
+
+        return second;
+    }
+};
